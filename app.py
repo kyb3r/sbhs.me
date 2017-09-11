30a993442fdae01fd875592a08dc022c558c739a
verixx <abdurraqeeb53@gmail.com>
2017-09-11 19:37:26 +1000

    addded temp debugging markers

--- app.py
+++ app.py
@@ -17,29 +17,34 @@ def index():
 
 @app.route('/login')
 def login():
+    print('_______________BEFORE_AUTHORISATION_____________________')
+    print(session)
+    print('____________________________________')
     sbhs = OAuth2Session(client_id)
     authorization_url, state = sbhs.authorization_url(auth_base_url)
-    print('____________________________________')
-    print(state)
     session['oauth_state'] = state
+    print('_______________AFTER_AUTHORISATION_____________________')
     print(session)
     print('____________________________________')
     return redirect(authorization_url)
 
 @app.route('/callback', methods=['GET'])
 def callback():
-    print('____________________________________')
+    print('______________Before_FETCH_TOKEN_________________')
     print(session)
     print('____________________________________')
     sbhs = OAuth2Session(client_id, state=session['oauth_state'])
     token = sbhs.fetch_token(token_url, client_secret=client_secret,
                                authorization_response=request.url)
     session['oauth_token'] = token['access_token']
+    print('______________After_FETCH_TOKEN_________________')
+    print(session)
+    print('____________________________________')
     return redirect(url_for('.profile'))
 
 @app.route("/profile", methods=["GET"])
 def profile():
-    print('____________________________________')
+    print('_______________BEFORE_PROFILE_REDIRECT___________________')
     print(session)
     print('____________________________________')
     sbhs = OAuth2Session(client_id, token=session['oauth_token'])
