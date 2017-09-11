91744cfb5ac9a131a9cf378db8b782352ce154ad
verixx <abdurraqeeb53@gmail.com>
2017-09-11 12:48:21 +1000

    deleted unnescessary stuff for final test

--- app.py
+++ app.py
@@ -6,64 +6,10 @@ import os
 app = Flask(__name__)
 
 
-# This information is obtained upon registration of a new GitHub OAuth
-# application here: https://github.com/settings/applications/new
-client_id = "f2a6748155100cf3e1f0"
-client_secret = "072d1db677c29eb1767f696bfd5a1b0174f8aa2e"
-authorization_base_url = 'https://github.com/login/oauth/authorize'
-token_url = 'https://github.com/login/oauth/access_token'
-
-
 @app.route("/")
 def demo():
-    """Step 1: User Authorization.
-
-    Redirect the user/resource owner to the OAuth provider (i.e. Github)
-    using an URL with a few key OAuth parameters.
-    """
-    github = OAuth2Session(client_id)
-    authorization_url, state = github.authorization_url(authorization_base_url)
-
-    # State is used to prevent CSRF, keep this for later.
-    session['oauth_state'] = state
-    return redirect(authorization_url)
-
-
-# Step 2: User authorization, this happens on the provider.
-
-@app.route("/callback", methods=["GET"])
-def callback():
-    """ Step 3: Retrieving an access token.
-
-    The user has been redirected back from the provider to your registered
-    callback URL. With this redirection comes an authorization code included
-    in the redirect URL. We will use that to obtain an access token.
-    """
-
-    github = OAuth2Session(client_id, state=session['oauth_state'])
-    token = github.fetch_token(token_url, client_secret=client_secret,
-                               authorization_response=request.url)
-
-    # At this point you can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token
-    # in /profile.
-    session['oauth_token'] = token
-
-    return redirect(url_for('.profile'))
-
-
-@app.route("/profile", methods=["GET"])
-def profile():
-    """Fetching a protected resource using an OAuth 2 token.
-    """
-    github = OAuth2Session(client_id, token=session['oauth_token'])
-    return jsonify(github.get('https://api.github.com/user').json())
+    return '<h1>Hello this is a website!!!!! WOWOWOWOWOWO! c=====3</h1>'
 
 
 if __name__ == "__main__":
-    # This allows us to use a plain HTTP callback
-    os.environ['DEBUG'] = "1"
-
-    app.secret_key = os.urandom(24)
-    print(os.environ)
-    app.run(debug=True, port=int(os.environ.get('PORT')))
+    app.run(debug=True, port=os.environ.get('PORT'))
