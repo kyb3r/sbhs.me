db2b544fad4ef8093be07384544ff818c03e4dd7
verixx <abdurraqeeb53@gmail.com>
2017-09-11 17:48:24 +1000

    Made a cooler html thingy

--- app.py
+++ app.py
@@ -1,56 +1,12 @@
-from requests_oauthlib import OAuth2Session
-from flask import Flask, request, redirect, session, url_for
-from flask.json import jsonify
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def demo():
-    code = '''
-<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
-<style>
-  .red-text {
-    color: red;
-  }
-
-  h2 {
-    font-family: Lobster, Monospace;
-  }
-
-  p {
-    font-size: 16px;
-    font-family: Monospace;
-  }
-
-  .thick-green-border {
-    border-color: green;
-    border-width: 10px;
-    border-style: solid;
-    border-radius: 50%;
-  }
-
-  .smaller-image {
-    width: 100px;
-  }
-</style>
-
-<h2 class="red-text">CatPhotoApp</h2>
-
-<p>Click here for <a href="#">cat photos</a>.</p>
-
-<a href="#"><img class="smaller-image thick-green-border" alt="A cute orange cat lying on its back. " src="https://bit.ly/fcc-relaxing-cat"></a>
-
-<p>Things cats love:</p>
-<ul>
-  <li>cat nip</li>
-  <li>laser pointers</li>
-  <li>lasagna</li>
-</ul>
-<p>Top 3 things cats hate:</p>
-'''
-    return code
+def index():
+    return open('tests.html').read()
 
 
 if __name__ == "__main__":
